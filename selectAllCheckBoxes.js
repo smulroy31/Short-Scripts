9d9972d061c6e5ec724fb17e18313edbd99f6ea2
SMulroy <smulroy31@gmail.com>
2022-10-21 10:41:20 -0700

    Applied spacing

--- selectAllCheckBoxes.js
+++ selectAllCheckBoxes.js
@@ -1,5 +1,5 @@
 //Select All Checkboxes On Page
 allSelects = document.querySelectorAll('input[type="checkbox"]');
-for(i=0;i<allSelects.length;i++){
-allSelects[i].click();
+    for(i=0;i<allSelects.length;i++){
+    allSelects[i].click();
 }
