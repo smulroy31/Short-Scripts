426df2deac39f5123f3628ac302afee0034184bd
Sean D. Mulroy <116298141+smulroy31@users.noreply.github.com>
2023-12-20 10:13:52 -0800

    Add files via upload

--- playlist_dl.py
+++ playlist_dl.py
@@ -1,3 +1,6 @@
-program = "yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 96K --yes-playlist "
+# Importing required module
+import os
+program = "sudo yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 96K --yes-playlist "
 url = input('Enter Playlist URL: ')
-exec(program + url)
+print("...Running Program...")
+os.system(program+url)
